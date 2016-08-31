69954bfb562aa646c0e8010d70d762e21f65bdf0
JulianaFigueira <juliana.f@samsung.com>
2016-08-31 13:21:31 -0400

    Physics World singleton

--- gvr-bullet/app/src/main/java/org/gearvrf/gvrbullet/BulletSampleMain.java
+++ gvr-bullet/app/src/main/java/org/gearvrf/gvrbullet/BulletSampleMain.java
@@ -1,13 +1,6 @@
 package org.gearvrf.gvrbullet;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Map;
 
 import org.gearvrf.FutureWrapper;
 import org.gearvrf.GVRAndroidResource;
@@ -18,7 +11,6 @@ import org.gearvrf.GVRMesh;
 import org.gearvrf.GVRMeshCollider;
 import org.gearvrf.GVRScene;
 import org.gearvrf.GVRSceneObject;
-import org.gearvrf.GVRTransform;
 import org.gearvrf.physics.GVRPhysicsWorld;
 import org.gearvrf.physics.GVRRigidBody;
 import org.gearvrf.utility.Log;
@@ -38,7 +30,6 @@ import android.graphics.Color;
 public class BulletSampleMain extends GVRMain {
 
     private GVRContext mGVRContext = null;
-    private ArrayList<GVRSceneObject> sceneObjects = new ArrayList<GVRSceneObject>();
     private static final float CUBE_MASS = 0.01f;
     private GVRScene mScene;
 
@@ -48,27 +39,22 @@ public class BulletSampleMain extends GVRMain {
 
         mScene = mGVRContext.getNextMainScene();
 
-        mScene.setPhysicsWorld(new GVRPhysicsWorld(mGVRContext));
-
         GVRCameraRig mainCameraRig = mScene.getMainCameraRig();
         mainCameraRig.getLeftCamera().setBackgroundColor(Color.BLACK);
         mainCameraRig.getRightCamera().setBackgroundColor(Color.BLACK);
-
         mainCameraRig.getTransform().setPosition(0.0f, 6.0f, 0.0f);
 
         /*
          * Create the ground. A simple textured quad. In bullet it will be a
          * plane shape with 0 mass
          */
-        GVRSceneObject groundScene = quadWithTexture(100.0f, 100.0f,
-                "floor.jpg");
+        GVRSceneObject groundScene = quadWithTexture(100.0f, 100.0f, "floor.jpg");
         groundScene.getTransform().setRotationByAxis(-90.0f, 1.0f, 0.0f, 0.0f);
         groundScene.getTransform().setPosition(0.0f, -100.0f, 0.0f);
         groundScene.attachCollider(new GVRMeshCollider(mGVRContext, groundScene.getRenderData().getMesh()));
-        groundScene.attachRigidBody(new GVRRigidBody(mGVRContext, 0, groundScene.getCollider(), groundScene.getTransform()));
+        groundScene.attachComponent(new GVRRigidBody(mGVRContext, 0, groundScene.getCollider(), groundScene.getTransform()));
 
         mScene.addSceneObject(groundScene);
-        sceneObjects.add(groundScene);
         /*
          * Create Some cubes in Bullet world and hit it with a sphere
          */
@@ -111,11 +97,7 @@ public class BulletSampleMain extends GVRMain {
 
     @Override
     public void onStep() {
-        Log.d("DEBUG" ,"ONSTEP");
-        mScene.getPhysicsWorld().step(1.0f/60.0f);
-        for(GVRSceneObject obj :  sceneObjects){
-             obj.getTransform();
-        }
+        GVRPhysicsWorld.step();
     }
 
     private GVRSceneObject quadWithTexture(float width, float height,
@@ -154,11 +136,9 @@ public class BulletSampleMain extends GVRMain {
         GVRSceneObject cubeObject = meshWithTexture("cube.obj", "cube.jpg");
         cubeObject.attachCollider(new GVRMeshCollider(mGVRContext, cubeObject.getRenderData().getMesh()));
         cubeObject.getTransform().setPosition(x, y, z);
-
-        cubeObject.attachRigidBody(new GVRRigidBody(mGVRContext, mass, cubeObject.getCollider(), cubeObject.getTransform()));
+        cubeObject.attachComponent(new GVRRigidBody(mGVRContext, mass, cubeObject.getCollider(), cubeObject.getTransform()));
 
         scene.addSceneObject(cubeObject);
-        sceneObjects.add(cubeObject);
     }
 
     /*
@@ -168,15 +148,12 @@ public class BulletSampleMain extends GVRMain {
     private void addSphere(GVRScene scene, float radius, float x, float y,
             float z, float mass) {
 
-        GVRSceneObject sphereObject = meshWithTexture("sphere.obj",
-                "sphere.jpg");
+        GVRSceneObject sphereObject = meshWithTexture("sphere.obj", "sphere.jpg");
         sphereObject.attachCollider(new GVRMeshCollider(mGVRContext, sphereObject.getRenderData().getMesh()));
         sphereObject.getTransform().setPosition(x, y, z);
-
-        sphereObject.attachRigidBody(new GVRRigidBody(mGVRContext, mass, sphereObject.getCollider(), sphereObject.getTransform()));
+        sphereObject.attachComponent(new GVRRigidBody(mGVRContext, mass, sphereObject.getCollider(), sphereObject.getTransform()));
 
         scene.addSceneObject(sphereObject);
-        sceneObjects.add(sphereObject);
     }
 
 }
